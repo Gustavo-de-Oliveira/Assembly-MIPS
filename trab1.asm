f8c56a9acef266298f844921d9f631154f2bd1fd
Matheus Sanchez <matheus2.sanchez@usp.br>
2020-03-22 19:22:39 -0300

    Time Full unido codando horresres tudo funfonando que orgulho <3

--- trab1.asm
+++ trab1.asm
@@ -2,11 +2,24 @@
 menuInicial:  .asciiz "Digite sua primeira opção(M|C):  " 
 str1:  .asciiz "\nOpção Escolhida (M|C):  " 
 menu1:  .asciiz "\nDigite a operação:  "
+strN1: .asciiz "\nDigite o primeiro numero: "
+strN2: .asciiz "Digite o segundo numero: "
+strResultado: .asciiz "O resultado: "
 
+
+teste:  .asciiz " STRING DE TESTE !"
 strAdicao:  .asciiz "\nEstá é uma operação de soma !"
+strPotencia:  .asciiz "\nEstá é uma operação de potencia !"
 strSubtracao:  .asciiz "\nEstá é uma operação de subtração !"
 strDivisao:  .asciiz "\nEstá é uma operação de divisao !"
 strMultiplicacao: .asciiz "\nEstá é uma operação de multiplicação !"
+strRaiz: .asciiz "\n Está é uma operação de Raiz: "
+strErrorDiv: .asciiz "\nNão pode dividir por 0 !"
+strErrorRaiz: .asciiz "Número < 0 !"
+
+
+n0: .float 0.0
+n1: .float 1.0
 
 .text
 .globl main
@@ -40,31 +53,190 @@ main:
  	
 
 #Podemos comparar o valor de $v0 com algum caracter
+
+
 ifMenu1:
 	beq $t1, '+', ADICAO
 	beq $t1, '-', SUBTRACAO
 	beq $t1, '/', DIVISAO
 	beq $t1, '*', MUTIPLICACAO
+	beq $t1, 'R', RAIZ
+	beq $t1, 'P', POTENCIA
+	
 	j fim 
 	
 ADICAO: 
-	la $a0, strAdicao # Primeiro Menu
-	jal printStr # print
+	la $a0, strAdicao # operação de soma
+	jal printStr # print	
+	
+	
+	la $a0, strN1
+	jal printStr
+	
+	#leitura primeiro num
+	jal leituraFloat
+	mov.s $f1, $f0
+	
+	la $a0, strN2
+	jal printStr
+	
+	#leitura segundo num
+	jal leituraFloat
+	mov.s $f2, $f0
+	add.s $f3, $f1, $f2
+	
+	la $a0, strResultado
+	jal printStr
+	jal printFloat
+	
 	j fim
+		
 	
 SUBTRACAO: 
-	la $a0, strSubtracao # Primeiro Menu
-	jal printStr # print
+	la $a0, strSubtracao # operação de soma
+	jal printStr # print	
+	
+	
+	la $a0, strN1
+	jal printStr
+	
+	#leitura primeiro num
+	jal leituraFloat
+	mov.s $f1, $f0
+	
+	la $a0, strN2
+	jal printStr
+	
+	#leitura segundo num
+	jal leituraFloat
+	mov.s $f2, $f0
+	sub.s $f3, $f1, $f2
+	
+	la $a0, strResultado
+	jal printStr
+	jal printFloat
+	
 	j fim
 	
 DIVISAO: 
-	la $a0, strDivisao # Primeiro Menu
+	la $a0, strDivisao # operação de soma
+	jal printStr # print	
+	
+	
+	la $a0, strN1
+	jal printStr
+	
+	#leitura primeiro num
+	jal leituraFloat
+	mov.s $f1, $f0
+	
+	la $a0, strN2
+	jal printStr
+	
+	#leitura segundo num
+	jal leituraFloat
+	mov.s $f2, $f0
+	
+	lwc1 $f8, n0
+	c.eq.s $f2, $f8
+	bc1t  errorDiv
+	
+	div.s $f3, $f1, $f2
+	la $a0, strResultado
+	jal printStr
+	jal printFloat
+	
+	j fim
+	
+	
+POTENCIA: 
+	la $a0, strPotencia # operação de soma
+	jal printStr # print	
+	
+	
+	la $a0, strN1
+	jal printStr
+	
+	#leitura primeiro num
+	jal leituraFloat
+	mov.s $f1, $f0
+	
+	la $a0, strN2
+	jal printStr
+	
+	#leitura segundo num
+	jal leituraFloat
+	mov.s $f2, $f0
+	
+	
+	
+		
+	lwc1 $f3, n1
+	lwc1 $f7, n1
+	lwc1 $f8, n0
+	
+	
+	c.eq.s $f2, $f8	
+	bc1t fimPotencia
+	
+
+		loop_potencia:
+			# multiplicacao	
+			mul.s $f3, $f3, $f1 #f3 -> 2 * 2 * 2
+			sub.s $f2, $f2, $f7 #f2 ->  0
+		
+			#Se t2 n�o for 0
+		
+			c.eq.s $f2, $f8	
+		bc1f loop_potencia
+
+	fimPotencia:
+	
+	la $a0, strResultado
+	jal printStr
+	jal printFloat
+	
+	j fim
+	
+	
+errorDiv:
+	la $a0, strErrorDiv
+	jal printStr
+	j fim
+	
+	
+MUTIPLICACAO:
+	la $a0, strMultiplicacao 
 	jal printStr # print
 	j fim
 	
-MUTIPLICACAO:
-	la $a0, strMultiplicacao # Primeiro Menu
-	jal printStr # print
+RAIZ: 
+	la $a0, strRaiz # operação de soma
+	jal printStr # print	
+	
+	la $a0, strN1
+	jal printStr
+	
+	#leitura primeiro num
+	jal leituraFloat
+	mov.s $f1, $f0
+	
+	#if (F1 < 0)
+	lwc1 $f7, n0
+	c.lt.s $f1, $f7
+	bc1t  errorRaiz
+	
+	sqrt.s $f3, $f1	
+	
+	la $a0, strResultado
+	jal printStr
+	jal printFloat
+	
+	j fim
+
+errorRaiz:
+	la $a0, strErrorRaiz
+	jal printStr
 	j fim
 	
 
@@ -73,8 +245,19 @@ leitura1char:
  	syscall
  	move $t1, $v0 #move de volta o char para A0
  	jr $ra 
+ 
+#leitura de float no $F0
+leituraFloat:
+	li $v0, 6
+ 	syscall
+ 	jr $ra 
  	
- 	
+ #print do que tem no registrador $F12	
+printFloat:
+	mov.s $f12, $f3
+	li $v0, 2
+	syscall 
+	jr $ra	
  
  #print do que tem no registrador $a0	
 printStr:
